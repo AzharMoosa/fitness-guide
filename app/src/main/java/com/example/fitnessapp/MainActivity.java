2bf0e2f5fec80bffbf494070008cbaef1dd02488
Azhar Moosa <azhar.moosa8@gmail.com>
2021-06-20 21:52:59 +0100

    Login Design UI

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginScreen(View v){
+        startActivity(new Intent(MainActivity.this, Login.class));
     }
 }
