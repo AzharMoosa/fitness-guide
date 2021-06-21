c7557d678ecb94bef8ad8ade4c42d24b09751763
Azhar Moosa <azhar.moosa8@gmail.com>
2021-06-21 17:50:23 +0100

    Check Token

--- app/src/main/java/com/example/fitnessapp/Login.java
+++ app/src/main/java/com/example/fitnessapp/Login.java
@@ -3,7 +3,9 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -15,10 +17,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.fitnessapp.user.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,60 +26,72 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.example.fitnessapp.constants.Constants.API_URL;
-import static com.example.fitnessapp.constants.Constants.AUTH;
-import static com.example.fitnessapp.constants.Constants.EMAIL;
-import static com.example.fitnessapp.constants.Constants.NAME;
-import static com.example.fitnessapp.constants.Constants.PASSWORD;
-import static com.example.fitnessapp.constants.Constants.USERS;
+import static com.example.fitnessapp.constants.Constants.*;
 
 public class Login extends AppCompatActivity {
 
-    private EditText email;
-    private EditText password;
+  private EditText email;
+  private EditText password;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        initialise();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    initialise();
+  }
 
-    private void initialise() {
-        email = findViewById(R.id.email_login);
-        password = findViewById(R.id.password_login);
-    }
+  private void initialise() {
+    email = findViewById(R.id.email_login);
+    password = findViewById(R.id.password_login);
+  }
 
-    public void signUpScreen(View v) {
-        startActivity(new Intent(Login.this, SignUp.class));
-    }
+  public void signUpScreen(View v) {
+    startActivity(new Intent(Login.this, SignUp.class));
+  }
 
-    public void loginUser(View v) throws JSONException {
-        // Retrieve Input
-        String userEmail = email.getText().toString();
-        String userPassword = password.getText().toString();
+  public void loginUser(View v) throws JSONException {
+    // Retrieve Input
+    String userEmail = email.getText().toString();
+    String userPassword = password.getText().toString();
 
-        // Create User
-        JSONObject data = new JSONObject();
-        data.put(EMAIL, userEmail);
-        data.put(PASSWORD, userPassword);
-        login(AUTH, data);
-    }
+    // Create User
+    JSONObject data = new JSONObject();
+    data.put(EMAIL, userEmail);
+    data.put(PASSWORD, userPassword);
+    login(AUTH, data);
+  }
 
-    private void login(String URL, JSONObject data) {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, API_URL + URL,
-                data, response -> Toast.makeText(getApplicationContext(), "Response:  " + response.toString(), Toast.LENGTH_SHORT).show(), error -> Toast.makeText(getApplicationContext(), "Error:  " + error.toString(), Toast.LENGTH_SHORT).show()) {
-            public Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("Content-Type", "application/json");
-                return params;
-            }
-        };
+  private void login(String URL, JSONObject data) {
+    RequestQueue requestQueue = Volley.newRequestQueue(this);
+    JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, API_URL + URL,
+        data, response -> {
+      try {
+        String token = response.getString(TOKEN);
+        startActivity(new Intent(Login.this, Dashboard.class));
+        saveToken(token);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }, error -> Toast
+        .makeText(getApplicationContext(), "Error: Email Or Password Not Valid", Toast.LENGTH_SHORT)
+        .show()) {
+      public Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put("Content-Type", "application/json");
+        return params;
+      }
+    };
+    int socketTimeout = 10000;
+    RetryPolicy policy = new DefaultRetryPolicy(socketTimeout,
+        DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+    objectRequest.setRetryPolicy(policy);
+    requestQueue.add(objectRequest);
+  }
 
-        int socketTimeout = 10000;
-        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-        objectRequest.setRetryPolicy(policy);
-        requestQueue.add(objectRequest);
-    }
+  private void saveToken(String token) {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    SharedPreferences.Editor editor = preferences.edit();
+    editor.putString(TOKEN, token);
+    editor.apply();
+  }
 }
--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,25 +1,73 @@
 package com.example.fitnessapp;
 
+import static com.example.fitnessapp.constants.Constants.API_URL;
+import static com.example.fitnessapp.constants.Constants.AUTH;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.RetryPolicy;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  private boolean isLogged = false;
 
-    public void loginScreen(View v){
-        startActivity(new Intent(MainActivity.this, Login.class));
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    try {
+      validToken();
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void signUpScreen(View v){
-        startActivity(new Intent(MainActivity.this, SignUp.class));
-    }
+  private void validToken() throws JSONException {
+    RequestQueue requestQueue = Volley.newRequestQueue(this);
+    JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, API_URL + AUTH,
+        null, response -> {
+      startActivity(new Intent(MainActivity.this, Dashboard.class));
+      isLogged = true;
+    }, error -> setContentView(R.layout.activity_main)) {
+      @Override
+      public Map<String, String> getHeaders() throws AuthFailureError {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("x-auth-token", getToken());
+        return headers;
+      }
+    };
+    int socketTimeout = 10000;
+    RetryPolicy policy = new DefaultRetryPolicy(socketTimeout,
+        DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+    objectRequest.setRetryPolicy(policy);
+    requestQueue.add(objectRequest);
+  }
+
+  private String getToken() {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    return preferences.getString("token", "");
+  }
+
+  public void loginScreen(View v) {
+    startActivity(new Intent(MainActivity.this, Login.class));
+  }
+
+  public void signUpScreen(View v) {
+    startActivity(new Intent(MainActivity.this, SignUp.class));
+  }
 }
